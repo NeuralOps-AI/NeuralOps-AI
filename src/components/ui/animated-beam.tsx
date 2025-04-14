65d3e24a8bbf02087d632027c28b59009c7f5e95
janareddyc7 <avatanishq00@gmail.com>
2025-04-14 17:01:04 -0400

    vercel error fixed

--- src/components/ui/animated-beam.tsx
+++ src/components/ui/animated-beam.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { cn } from "@/utils";
+import { cn } from "../../utils/cn";
 import { motion } from "framer-motion";
 import { RefObject, useEffect, useId, useState } from "react";
 
